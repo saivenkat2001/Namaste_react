8ab5f36866df4de3f7e7e1c432e038fc95aec0f0
saivenkat2001 <saivenkat.k@aifalabs.com>
2024-03-13 16:34:34 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-const heading = React.createElement("h1",{
-    id:"heading"
-},"Hello World");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello World");
+// const heading = <h1 className="heading">Hello World!!!!</h1>;
+
+const Title =()=>(
+    <h1>Hello World!!!</h1>
+);
+
+const Heading = ()=>(
+    <div>
+        <Title/>
+        <h2>Hiiiii</h2>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading/>);
